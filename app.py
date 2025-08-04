fc213c1c50e2180dc51320f13f7fbdda2c051a42
learninghe <99811981+learninghe@users.noreply.github.com>
2025-08-04 15:08:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ aa_only = re.compile(r'[ACDEFGHIKLMNPQRSTVWY]', flags=re.I)
 
 # 标题
 st.title("肽段序列匹配工具")
-st.write("上传数据文件注意：后缀必须为.xlsx，.xlsx文件内容必须包含标题行且数据在第一列，标题行内容必须为Peptide，后续行依次接要分析的肽段")
+st.write("上传数据文件注意：文件后缀必须为.xlsx，.xlsx文件内容必须包含标题行，且所有数据必须在第一列，第一列第一行的标题行内容必须为Peptide，后续行依次接要分析的肽段")
 st.write("上传 Excel 文件，自动匹配功能肽数据库并返回结果")
 
 # 上传文件
@@ -83,4 +83,5 @@ if uploaded_file:
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
     )
+
 
