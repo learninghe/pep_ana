855fc6e3e9b029e4e7e014b5ecf0d9ac915475e2
learninghe <99811981+learninghe@users.noreply.github.com>
2025-08-04 15:05:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ aa_only = re.compile(r'[ACDEFGHIKLMNPQRSTVWY]', flags=re.I)
 
 # 标题
 st.title("肽段序列匹配工具")
+st.title("上传数据文件注意：后缀必须为.xlsx，.xlsx文件内容必须包含标题行且数据在第一列，标题行内容必须为Peptide，后续行依次接要分析的肽段")
 st.write("上传 Excel 文件，自动匹配功能肽数据库并返回结果")
 
 # 上传文件
@@ -80,4 +81,5 @@ if uploaded_file:
         data=to_excel(output_df),
         file_name='肽段匹配结果.xlsx',
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
     )
